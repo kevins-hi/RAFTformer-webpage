8c2021b9e5419e9f65d7312ad1181b96b457c2d7
kevins-hi <kevinxpshi@berkeley.edu>
2023-06-12 12:53:26 -0700

    finished draft 2

--- script.js
+++ script.js
@@ -13,3 +13,19 @@
       open = !open;
     });
   })(document);
+
+  (function(document){
+    var div = document.getElementById('dropdown-container-2');
+    var icon = document.getElementById('caret-icon-2');
+    var open = false;
+    
+    div.addEventListener('click', function(){
+      if(open){
+        icon.className = 'fas fa-caret-down fa-2xs';  
+      } else{
+        icon.className = 'fas fa-caret-down fa-2xs open';
+      }
+      
+      open = !open;
+    });
+  })(document);
